31027ac5e3652b8afa759c2b1a2fba246741a797
admin <nogerov2000@bk.ru>
2024-03-20 16:32:05 +0300

    Base

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
+const MiniCssExractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js'
+    },
+    plugins: [new MiniCssExractPlugin],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExractPlugin.loader,
+                        options: {
+                            esModule: true,
+                        },
+                    },
+                    'css-loader',
+                ]
+            }
+        ]
     }
 }
