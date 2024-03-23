be4ddabcb9c1df228ff8b2686ce1ab553c0065cd
admin <nogerov2000@bk.ru>
2024-03-23 23:09:12 +0300

    Final Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,37 @@
 const MiniCssExractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin')
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devServer: {
+        static: './dist',
+        hot: true,
+        port: 8081,
+        open: true,
+    },
+    stats: {
+        children: false,
+        entrypoints: true,
+    },
+    devtool: 'inline-source-map',
     output: {
         filename: 'main.js'
     },
     plugins: [ 
         new MiniCssExractPlugin(), 
         new HtmlWebpackPlugin({options: {
+            title: 'Development',
             template: "../NPM/src/index.pug",
             filename: "index.html",
         }
         }),
+        new ESLintPlugin({files: './src/**/*.{vue,scss}'}),
+        new StylelintPlugin({files: './src/**/*.{vue,scss}'})
     ],
 
     module: {
@@ -35,5 +53,20 @@ module.exports = {
                 use: 'pug-loader'
             }
         ]
-    }
-}
+    },
+
+    optimization: {
+        minimizer: true,
+        minimizer: [
+            new TerserWebpackPlugin(),
+            new CssMinimizerPlugin({minimizerOptions: {
+                preset: [
+                    'default',
+                    {
+                        discardComments: {removeAll: true}
+                    },   
+            ]
+            }})
+        ]
+    },
+};
